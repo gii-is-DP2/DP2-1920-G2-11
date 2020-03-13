6cd21ab8245151179dd06d761320e195a5b757d2
TomasJP95 <tomjimpon@alum.us.es>
2020-03-13 18:18:32 +0100

    Creación entidades

---
+++ src/main/java/org/springframework/samples/petclinic/model/Vaccine.java
@@ -0,0 +1,85 @@
+
+package org.springframework.samples.petclinic.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "vaccines")
+public class Vaccine extends NamedEntity {
+
+	@NotBlank
+	@Column(name = "name")
+	private String		name;
+
+	@NotBlank
+	@Column(name = "components")
+	private String		components;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "age")
+	private Date		age;
+
+	@ManyToOne
+	@Valid
+	@JoinColumn(name = "sickness_id")
+	private Sickness	sickness;
+
+	@OneToOne
+	@Valid
+	private PetType		petType;
+
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getComponents() {
+		return this.components;
+	}
+
+	public void setComponents(final String components) {
+		this.components = components;
+	}
+
+	public Date getAge() {
+		return this.age;
+	}
+
+	public void setAge(final Date age) {
+		this.age = age;
+	}
+
+	public Sickness getSickness() {
+		return this.sickness;
+	}
+
+	public void setSickness(final Sickness sickness) {
+		this.sickness = sickness;
+	}
+
+	public PetType getPetType() {
+		return this.petType;
+	}
+
+	public void setPetType(final PetType petType) {
+		this.petType = petType;
+	}
+
+}
