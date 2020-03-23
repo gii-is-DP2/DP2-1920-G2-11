5393c3fa568c6860ce95d59b662c2d18c5d2092c
Feltrimar <32713205+Feltrimar@users.noreply.github.com>
2020-03-24 00:28:40 +0100

    Product Type HU19
    Iteración 1, mas o menos

---
+++ src/main/java/org/springframework/samples/petclinic/web/ProductTypeController.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ProductTypeService;
+import org.springframework.samples.petclinic.model.ProductTypes;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * @author Juergen Hoeller
+ * @author Mark Fisher
+ * @author Ken Krebs
+ * @author Arjen Poutsma
+ */
+@Controller
+public class ProductTypeController {
+
+	private final ProductTypeService productTypeService;
+
+	@Autowired
+	public ProductTypeController(ProductTypeService productTypeService) {
+		this.productTypeService = productTypeService;
+	}
+
+	@GetMapping(value = { "/productTypes" })
+	public String showProductTypeList(Map<String, Object> model) {
+		// Here we are returning an object of type 'ProductTypes' rather than a collection of ProductType
+		// objects
+		// so it is simpler for Object-Xml mapping
+		ProductTypes productTypes = new ProductTypes();
+		productTypes.getProductTypeList().addAll(this.productTypeService.findProductTypes());
+		model.put("productTypes", productTypes);
+		return "productTypes/productTypeList";
+	}
+
+	@GetMapping(value = { "/productTypes.xml"})
+	public @ResponseBody ProductTypes showResourcesProductTypeList() {
+		// Here we are returning an object of type 'ProductTypes' rather than a collection of ProductType
+		// objects
+		// so it is simpler for JSon/Object mapping
+		ProductTypes productTypes = new ProductTypes();
+		productTypes.getProductTypeList().addAll(this.productTypeService.findProductTypes());
+		return productTypes;
+	}
+
+}
