2fcad8a2c0a5ada3f814d4c4b43992ca1f664682
Jose Carlos <Jose Carlos@CASA>
2020-03-30 20:48:40 +0200

    Rama actualizada

--- src/test/java/org/springframework/samples/petclinic/web/SicknessControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/SicknessControllerTest.java
@@ -1,6 +1,55 @@
+
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.Sickness;
+import org.springframework.samples.petclinic.service.SicknessService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@WebMvcTest(controllers = SicknessController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class SicknessControllerTest {
+
+	@Autowired
+	private SicknessController	sicknessController;
+
+	@MockBean
+	private SicknessService		sicknessService;
+
+	@Autowired
+	private MockMvc				mockMvc;
+
+
+	@BeforeEach
+	void setup() {
+
+		Sickness otitis = new Sickness();
+		otitis.setName("Otitis");
+		otitis.setCause("Parásitos(ácaros), hongos, bacterias");
+		otitis.setSymptom("Inflamación del conjunto auditivo, dolor, pérdida de audición");
+		otitis.setSeverity(2);
+		otitis.setId(1);
+		PetType cat = new PetType();
+		cat.setId(1);
+		cat.setName("cat");
+	}
+
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowSicknessListHtml() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/sicknesses")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("sicknesses"))
+			.andExpect(MockMvcResultMatchers.view().name("sicknesses/sicknessList"));
+	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/web/VaccineControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VaccineControllerTest.java
@@ -1,6 +1,53 @@
+
 package org.springframework.samples.petclinic.web;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Sickness;
+import org.springframework.samples.petclinic.model.Vaccine;
+import org.springframework.samples.petclinic.service.VaccineService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+@WebMvcTest(controllers = VaccineController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class VaccineControllerTest {
 
+	@Autowired
+	private VaccineController	vaccineController;
+
+	@MockBean
+	private VaccineService		vaccineService;
+
+	@Autowired
+	private MockMvc				mockMvc;
+
+
+	@BeforeEach
+	void setup() {
+
+		Vaccine vacunaA = new Vaccine();
+		vacunaA.setName("Vacuna A");
+		vacunaA.setComponents("Q,W,E,R");
+		vacunaA.setMonths(4);
+		vacunaA.setId(1);
+		Sickness otitis = new Sickness();
+		otitis.setId(1);
+		otitis.setName("otitis");
+	}
+
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowVaccineListHtml() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/vaccines")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("vaccines"))
+			.andExpect(MockMvcResultMatchers.view().name("vaccines/vaccinesList"));
+	}
 }
