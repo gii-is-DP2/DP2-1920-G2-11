acb37b386dc21032c27cd1edc1359838fed125dd
Jose Carlos <Jose Carlos@CASA>
2020-04-11 20:01:56 +0200

    Rama actualizada

--- src/test/java/org/springframework/samples/petclinic/web/SicknessControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/SicknessControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.Sickness;
 import org.springframework.samples.petclinic.service.SicknessService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -33,17 +34,59 @@ public class SicknessControllerTest {
 	@Autowired
 	private MockMvc				mockMvc;
 
-	private static final int	TEST_PET_ID			= 1;
+	private static final int	TEST_PET_ID				= 1;
 
-	private static final int	TEST_SICKNESS_ID	= 1;
+	private static final int	TEST_PET_ERROR_ID		= 14;
+
+	private static final int	TEST_SICKNESS_ID		= 1;
+
+	private static final int	TEST_SICKNESS_ERROR_ID	= 30;
+
+	private static final int	TEST_PET_SHOW_ERROR_ID	= 14;
 
 
 	@BeforeEach
 	void setup() {
 		List<Sickness> sicknesses = new ArrayList<Sickness>();
+		Sickness rabia = new Sickness();
+		rabia.setId(3);
+		rabia.setName("Rabia");
+		rabia.setCause("Virus");
+		rabia.setSymptom("Irritabilidad, exceso de salivación, fiebre, vómitos");
+		rabia.setSeverity(1);
+		PetType cat = new PetType();
+		cat.setId(1);
+		cat.setName("cat");
+		rabia.setType(cat);
+		sicknesses.add(rabia);
+
+		//SHOW
 		Sickness sickness = new Sickness();
-		sicknesses.add(sickness);
+		sickness.setId(1);
+		sickness.setName("Otitis");
+		sickness.setCause("Parásitos(ácaros), hongos, bacterias");
+		sickness.setSymptom("Inflamación del conjunto auditivo, dolor, pérdida de audición");
+		sickness.setSeverity(2);
+		sickness.setType(cat);
+
+		//LIST ERROR
+		List<Sickness> sicknessesError = new ArrayList<Sickness>();
+
+		//SHOW ERROR
+		//		Sickness sicknessError = new Sickness();
+		//		sicknessError.setId(30);
+		//		sicknessError.setName("Pájaro Loco");
+		//		sicknessError.setSeverity(0);
+		//		PetType bird = new PetType();
+		//		bird.setId(5);
+		//		bird.setName("bird");
+		//		sicknessError.setType(bird);
+
 		BDDMockito.given(this.sicknessService.findSicknessesByPetId(SicknessControllerTest.TEST_PET_ID)).willReturn(sicknesses);
+		BDDMockito.given(this.sicknessService.findSicknessesById(SicknessControllerTest.TEST_SICKNESS_ID)).willReturn(sickness);
+		BDDMockito.given(this.sicknessService.findSicknessesByPetId(SicknessControllerTest.TEST_PET_ERROR_ID)).willReturn(sicknessesError);
+		//BDDMockito.given(this.sicknessService.findSicknessesById(SicknessControllerTest.TEST_SICKNESS_ERROR_ID)).willReturn(sicknessError);
+
 	}
 
 	@WithMockUser(value = "spring")
@@ -56,9 +99,8 @@ public class SicknessControllerTest {
 	@WithMockUser(value = "spring")
 	@Test
 	void testShowSicknessesErrorHtml() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/sicknesses", SicknessControllerTest.TEST_PET_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("sicknesses"))
-			.andExpect(MockMvcResultMatchers.view().name("sicknesses/sicknessError"));
-
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/sicknesses", SicknessControllerTest.TEST_PET_ERROR_ID)).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("sicknessesError")).andExpect(MockMvcResultMatchers.view().name("sicknesses/sicknessError"));
 	}
 
 	@WithMockUser(value = "spring")
@@ -67,5 +109,12 @@ public class SicknessControllerTest {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/sicknesses/{sicknessId}", SicknessControllerTest.TEST_PET_ID, SicknessControllerTest.TEST_SICKNESS_ID)).andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.model().attributeExists("sickness")).andExpect(MockMvcResultMatchers.view().name("sicknesses/sicknessShow"));
 	}
+
+	//	@WithMockUser(value = "spring")
+	//	@Test
+	//	void testShowSicknessesShowErrorHtml() throws Exception {
+	//		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/sicknesses/{sicknessId}", SicknessControllerTest.TEST_PET_SHOW_ERROR_ID, SicknessControllerTest.TEST_SICKNESS_ERROR_ID)).andExpect(MockMvcResultMatchers.status().isOk())
+	//			.andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("sicknessError")).andExpect(MockMvcResultMatchers.view().name("sicknesses/sicknessDetailsError"));
+	//	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/web/VaccineControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VaccineControllerTest.java
@@ -2,6 +2,7 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.Sickness;
 import org.springframework.samples.petclinic.model.Vaccine;
 import org.springframework.samples.petclinic.service.VaccineService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
@@ -40,14 +43,56 @@ public class VaccineControllerTest {
 	@BeforeEach
 	void setup() {
 
-		BDDMockito.given(this.vaccineService.findVaccinesBySicknessId(VaccineControllerTest.TEST_SICKNESS_ID)).willReturn(new ArrayList<Vaccine>());
+		List<Vaccine> vaccines = new ArrayList<Vaccine>();
+		Vaccine f = new Vaccine();
+		f.setId(6);
+		f.setName("Vacuna F");
+		f.setComponents("Componente A");
+		f.setMonths(12);
+		Vaccine f2 = new Vaccine();
+		f2.setId(7);
+		f2.setName("Vacuna F2");
+		f2.setComponents("Componente A");
+		f2.setMonths(18);
+		Sickness iF = new Sickness();
+		iF.setId(6);
+		iF.setName("Inmunodeficiencia felina");
+		iF.setCause("Lentivirus");
+		iF.setSymptom("Infecciones bucales, patologías respiratorias, infecciones intestinales");
+		iF.setSeverity(3);
+		PetType cat = new PetType();
+		cat.setId(1);
+		cat.setName("cat");
+		iF.setType(cat);
+		f.setSickness(iF);
+		f2.setSickness(iF);
+		vaccines.add(f);
+		vaccines.add(f2);
+
+		//SHOW
+		Vaccine vaccine = new Vaccine();
+		vaccine.setId(1);
+		vaccine.setName("Vacuna A");
+		vaccine.setComponents("Q,W,E,R");
+		vaccine.setMonths(4);
+		Sickness sickness = new Sickness();
+		sickness.setId(1);
+		sickness.setName("Otitis");
+		sickness.setCause("Parásitos(ácaros), hongos, bacterias");
+		sickness.setSymptom("Inflamación del conjunto auditivo, dolor, pérdida de audición");
+		sickness.setSeverity(2);
+		sickness.setType(cat);
+		vaccine.setSickness(sickness);
+
+		BDDMockito.given(this.vaccineService.findVaccinesBySicknessId(VaccineControllerTest.TEST_SICKNESS_ID)).willReturn(vaccines);
+		BDDMockito.given(this.vaccineService.findVaccineById(VaccineControllerTest.TEST_VACCINE_ID)).willReturn(vaccine);
 	}
 
 	@WithMockUser(value = "spring")
 	@Test
 	void testShowVaccineListHtml() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/owners/*/pets/{petId}/sicknesses/{sicknessId}/vaccines", VaccineControllerTest.TEST_PET_ID, VaccineControllerTest.TEST_SICKNESS_ID)).andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.model().attributeExists("vaccines")).andExpect(MockMvcResultMatchers.view().name("vaccines/vaccineList"));
+			.andExpect(MockMvcResultMatchers.model().attributeExists("vaccines")).andExpect(MockMvcResultMatchers.view().name("vaccines/vaccinesList"));
 
 	}
 
