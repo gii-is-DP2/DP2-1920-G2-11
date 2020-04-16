839b1bdfc4ae0854c4eb71ca199e8df2d6d11c50
lizseth <lizseth.esquen@gmail.com>
2020-04-16 16:14:24 +0200

    arreglo de fallos en Vaccine

--- src/test/java/org/springframework/samples/petclinic/escenarios/HU4Test.java
+++ src/test/java/org/springframework/samples/petclinic/escenarios/HU4Test.java
@@ -1,6 +1,8 @@
 
 package org.springframework.samples.petclinic.escenarios;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +22,14 @@ public class HU4Test {
 	//Caso positivo
 	@Test
 	void shouldFindVaccineWithCorrectId() {
-		Vaccine vaccine = this.vaccineRepository.findById(2);
-		Assertions.assertTrue(vaccine.getName().equals("Vacuna B") && vaccine.getComponents().equals("A,S,D,F") && vaccine.getMonths().equals(6) && vaccine.getSickness().getName().equals("Conjuntivitis"));
+		Optional<Vaccine> vaccine = this.vaccineRepository.findById(2);
+		Assertions.assertTrue(vaccine.get().getName().equals("Vacuna B") && vaccine.get().getComponents().equals("A,S,D,F") && vaccine.get().getMonths().equals(6) && vaccine.get().getSickness().getName().equals("Conjuntivitis"));
 	}
 
 	//Caso negativo
 	@Test
 	void shouldVaccineWithoutDetails() {
-		Vaccine vaccine = this.vaccineRepository.findById(8);
-		Assertions.assertTrue(vaccine.getName().equals("Vacunote") && vaccine.getComponents().isEmpty() && vaccine.getMonths().equals(0));
+		Optional<Vaccine> vaccine = this.vaccineRepository.findById(8);
+		Assertions.assertTrue(vaccine.get().getName().equals("Vacunote") && vaccine.get().getComponents().isEmpty() && vaccine.get().getMonths().equals(0));
 	}
 }
