d7bf87f6c55fae2135d92e53c2919b5fe50a2ed3
Jaime Lucas Lozano <jailucloz@alum.us.es>
2020-04-22 19:39:51 +0200

    nuevos tests productController

--- src/test/java/org/springframework/samples/petclinic/web/ProductControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/ProductControllerTests.java
@@ -2,7 +2,9 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.model.Clinic;
 import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.model.ProductType;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.samples.petclinic.service.ProductService;
 import org.springframework.samples.petclinic.service.ProductTypeService;
@@ -22,6 +26,9 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @WebMvcTest(controllers = ProductController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class ProductControllerTests {
@@ -44,37 +51,73 @@ public class ProductControllerTests {
 	private static final int	TEST_PRODUCT_ID	= 1;
 
 	private static final int	TEST_CLINIC_ID	= 1;
+	
+	private static final int	TEST_PRODUCT_TYPE_ID	= 1;
 
 
 	@BeforeEach
 	void setup() {
 		List<Product> products = new ArrayList<Product>();
 		Product product = new Product();
+		product.setDescription("para pelo seco");
+		product.setId(1);
+		product.setName("chámpu hidratante");
+		product.setPrice(4.00);
+		ProductType productType = new ProductType();
+		productType.setId(1);
+		productType.setName("Higiene");
+		product.setProductType(productType);
+		product.setStock(3);
+		Product product2 = new Product();
+		product2.setDescription("contiene láminas masticables para mantener limpia la dentadura del perro");
+		product2.setId(2);
+		product2.setName("Limpiador bucal");
+		product2.setPrice(8.00);
+		ProductType productType2 = new ProductType();
+		productType2.setId(1);
+		productType2.setName("Higiene");
+		product2.setProductType(productType);
+		product2.setStock(100);
+		
+		
 		products.add(product);
-		//BDDMockito.given(this.productService.findSicknessesByPetId(SicknessControllerTest.TEST_PET_ID)).willReturn(sicknesses);
+		products.add(product2);
+		
+		
+		
+		
+		
+		
 		BDDMockito.given(this.productService.findProductByClinicId(ProductControllerTests.TEST_CLINIC_ID)).willReturn(products);
+		BDDMockito.given(this.productService.findProductsByProductTypeId(ProductControllerTests.TEST_PRODUCT_TYPE_ID)).willReturn(products);
+		BDDMockito.given(this.productService.findProductsById(ProductControllerTests.TEST_PRODUCT_ID)).willReturn(product);
+		
+		
 	}
 
 	@WithMockUser(value = "spring")
 	@Test
-	void testShowProductsListHtml() throws Exception {
+	void testShowProductsList() throws Exception {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/clinics/{clinicId}/products", ProductControllerTests.TEST_CLINIC_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("products"))
+			.andExpect(MockMvcResultMatchers.view().name("products/clinicProductList"));
+	}
+
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowProduct() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/products/{productId}", ProductControllerTests.TEST_PRODUCT_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("product"))
+			.andExpect(MockMvcResultMatchers.view().name("products/productShow"));
+
+	}
+
+
+	
+	// devuelve productos filtrados por tipo
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testShowProducts() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/products/productType/{productTypeId}", ProductControllerTests.TEST_PRODUCT_TYPE_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("products"))
 			.andExpect(MockMvcResultMatchers.view().name("products/productList"));
 	}
-
-	@WithMockUser(value = "spring")
-	@Test
-	void testShowProductByClinicHtml() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("products/clinic/{clinicId}", ProductControllerTests.TEST_CLINIC_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("products"))
-			.andExpect(MockMvcResultMatchers.view().name("products/clinicProductList"));
-
-	}
-
-	@WithMockUser(value = "spring")
-	@Test
-	void testShowProductsHtml() throws Exception {
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/products/{productId}/", ProductControllerTests.TEST_PRODUCT_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("product"))
-			.andExpect(MockMvcResultMatchers.view().name("products/productShow"));
-	}
-
 }
