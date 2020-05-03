54b47a52620667c272673dd4908f2ca593ae8b08
Aureliano Piqueras Rodríguez <44163281+aurpiqrod@users.noreply.github.com>
2020-05-03 20:15:05 +0200

    Hu 19-20-21: Test de historias de usuario y Test del Servicio Medicine

---
+++ src/test/java/org/springframework/samples/petclinic/escenarios/HU20Test.java
@@ -0,0 +1,48 @@
+
+package org.springframework.samples.petclinic.escenarios;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Medicine;
+import org.springframework.samples.petclinic.model.Vaccine;
+import org.springframework.samples.petclinic.service.MedicineService;
+import org.springframework.samples.petclinic.service.VaccineService;
+import org.springframework.stereotype.Service;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+public class HU20Test {
+
+	@Autowired
+	private MedicineService medicineService;
+
+
+	//Caso positivo
+	@Test
+	void shouldFindCorrectMedicinesDetailsWithId() {
+		Collection<Medicine> meds = this.medicineService.findMedicines();
+		Medicine medicine = this.medicineService.findById(1);
+		Assertions.assertTrue(meds.contains(medicine)
+				&&	medicine.getName().equals("Medicina A")
+				&& medicine.getComponents().equals("Componente A")
+				&& medicine.getPetType().getId().equals(1)&&medicine.getPetType().getName().equals("cat")
+				&& medicine.getSickness().getId().equals(1)&&medicine.getSickness().getName().equals("Otitis")
+				&& medicine.getTreatment().equals("1 cada 8 horas"));
+	}
+
+	//Caso negativo
+	@Test
+	void shouldNotFindMedicinesDetailsWithIncorrectId() {
+		Collection<Medicine> meds = this.medicineService.findMedicines();
+		Medicine medicine = this.medicineService.findById(50);
+		Assertions.assertFalse(meds.contains(medicine));
+	}
+
+
+}
