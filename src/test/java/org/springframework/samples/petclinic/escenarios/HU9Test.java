a95bc109a30e911d4d6e03524db8cc80167eeac5
lizseth <lizseth.esquen@gmail.com>
2020-05-07 23:11:24 +0200

    cambios en la reoganizacion de las hu

--- src/test/java/org/springframework/samples/petclinic/escenarios/HU9Test.java
+++ src/test/java/org/springframework/samples/petclinic/escenarios/HU9Test.java
@@ -1,34 +1,35 @@
 
 package org.springframework.samples.petclinic.escenarios;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.petclinic.model.Clinic;
-import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.service.ProductService;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class HU9Test {
 
 	@Autowired
-	private ClinicService clinicService;
+	private ProductService productService;
 
 
 	//Caso positivo
 	@Test
-	void shouldFindClinicWithCorrectId() {
-		Clinic clinic = this.clinicService.findById(2);
-		Assertions.assertTrue(clinic.getName().equals("Veterinaria Nervión"));
+	void shouldFindProductWithCorrectClinicId() {
+		List<Product> products = this.productService.findProductByClinicId(2);
+		Assertions.assertTrue(!products.isEmpty());
 	}
 
 	//Caso negativo
 	@Test
-	void shouldClinicsEmpty() {
-		Clinic clinics = this.clinicService.findById(3);
-		Assertions.assertTrue(clinics.getName().equals("Clínica Los Arcos"));
-		Assertions.assertTrue(clinics.getAddress().isEmpty() && clinics.getTelephone().isEmpty() && clinics.getEmail().isEmpty() && clinics.getCity().isEmpty());
+	void shouldProductsEmpty() {
+		List<Product> product = this.productService.findProductByClinicId(3);
+		Assertions.assertTrue(product.isEmpty());
 	}
 }
