ce118a544a94e9e7a263f59ee68a00ef3b984a84
Feltrimar <32713205+Feltrimar@users.noreply.github.com>
2020-05-09 16:41:18 +0200

    update

--- src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
@@ -1,11 +1,16 @@
 
 package org.springframework.samples.petclinic.service;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.Product;
 import org.springframework.samples.petclinic.model.Product;
 import org.springframework.samples.petclinic.repository.ProductRepository;
 import org.springframework.stereotype.Service;
@@ -15,6 +20,9 @@ public class ProductServiceTests {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private ProductService productService;
 
 
 	@Test
@@ -26,5 +34,29 @@ public class ProductServiceTests {
 		Assertions.assertTrue(product.getProductType().getId().equals(1));
 		Assertions.assertTrue(product.getStock().equals(3));
 
+	}
+	
+	@Test
+	void createProductTest() {
+		Collection<Product> products = productService.findProducts();
+		int size= products.size();
+		Product product = new Product();
+		product.setId(31);
+		product.setName("Producto X");
+		product.setDescription("Descripcion X");
+		product.setPrice(2.0);
+		product.setStock(2);
+		this.productService.save(product);
+		Assertions.assertTrue(size<productService.findProducts().size());
+
+	}
+
+	@Test
+	void deleteProductTest() {
+		Collection<Product> products = productService.findProducts();
+		int size= products.size();
+		Product product = this.productService.findProductsById(1);
+		this.productService.delete(product);
+		Assertions.assertTrue(size>productService.findProducts().size());
 	}
 }
--- src/test/java/org/springframework/samples/petclinic/web/ProductControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/ProductControllerTests.java
@@ -20,6 +20,7 @@ import org.springframework.samples.petclinic.service.ProductService;
 import org.springframework.samples.petclinic.service.ProductTypeService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -113,5 +114,45 @@ public class ProductControllerTests {
 		this.mockMvc.perform(MockMvcRequestBuilders.get("/products")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("products"))
 			.andExpect(MockMvcResultMatchers.view().name("products/productList"));
 	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testDeleteProduct() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/products/delete/{productId}", ProductControllerTests.TEST_PRODUCT_ID)).andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+			.andExpect(MockMvcResultMatchers.view().name("products/productList"));
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testInitCreationForm() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/products/new")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("products/editProduct"));
+	}
 
+	//Error, tiene que ser algo del type
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormSuccess() throws Exception {
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.post("/products/save").with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "Comida").param("name", "Filete").param("price", "2.00").param("stock", "1"))
+			.andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).andExpect(MockMvcResultMatchers.view().name("products/productList"));
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormHasErrors1() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/products/save").with(SecurityMockMvcRequestPostProcessors.csrf()).param("name", "Filete")).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("product")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("product", "description"))
+			.andExpect(MockMvcResultMatchers.view().name("products/editProduct"));
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessCreationFormHasErrors2() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/products/save").with(SecurityMockMvcRequestPostProcessors.csrf()).param("description", "Comida").param("name", "Filete").param("stock", "a").param("price","A")).andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.model().attributeHasErrors("product")).andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("product", "price"))
+			.andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("product", "stock"))
+			.andExpect(MockMvcResultMatchers.view().name("products/editProduct"));
+	}
+
+	
 }
